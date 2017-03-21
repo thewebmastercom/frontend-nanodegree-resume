94d7a0348ad2183af0114c37f38089758e5626a9
Jonathan Griffin <jonathan@thewebmaster.com>
2017-03-21 21:25:29 +0000

    first commit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var awesomeThoughts = "I am Jonathan and I am AWESOME";
+console.log(awesomeThoughts)
+
+var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+$("#main").append(funThoughts);
